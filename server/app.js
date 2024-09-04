e852c858acf3559dc219ca1cf804ef1a29327335
Christophe Calvet <onlyatest@mytest.com>
2024-09-04 10:30:14 +0200

    done

--- server/app.js
+++ server/app.js
@@ -2,5 +2,17 @@ const express = require('express');
 const app = express();
 // Your code here
 
+const path = require('path');
+
+//Part1
+//app.use(express.static(path.join(__dirname, 'assets')));
+
+//Part 2 error in problem description. Replace ok test by http://localhost:5000/hello-world.js
+//app.use(express.static(path.join(__dirname, 'assets/scripts')));
+
+
+//Part 3
+app.use('/stylesheets',express.static(path.join(__dirname, 'assets/css')));
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
